eb7f982e1ef4d0a0161b80e5f1a9aed1c046d98f
Stijn Smekens <stijnsmekens@gmail.com>
2017-05-29 16:00:48 +0200

    add validations for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :hooker
   belongs_to :user
+  validates :rating, presence: true, inclusion: { in: (0..5) }, numericality: { only_integer: true }
+  validates :title, presence: true
 end
