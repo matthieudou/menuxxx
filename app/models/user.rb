cca4be2316b53bc845bd3747cdc81865df257a71
Stijn Smekens <stijnsmekens@gmail.com>
2017-05-29 16:50:47 +0200

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :hookers
   has_many :bookings
+  has_many :reviews
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :username, presence: true
+  validates :date_of_birth, presence: true
 end
