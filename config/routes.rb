443835d6feb017a66ccf8da36c3bbc3b15b7146b
thibaut gendebien <thibautgendebien@gmail.com>
2017-05-30 10:49:50 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :users, only: [ :create, :new, :edit, ]  do
+    resources :bookings
+    resources :hookers
+    resources :reviews
+  end
 end
