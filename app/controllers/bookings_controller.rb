8f8abfbed94d386d3af43fe2bebba439c1dbd11f
thibaut gendebien <thibautgendebien@gmail.com>
2017-05-30 17:00:42 +0200

    controller safe bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,47 +1,56 @@
 class BookingsController < ApplicationController
 
- before_action :set_hooker, only: [:create, :new]
+ before_action :set_hooker, only: [:create, :new, :edit]
 
-  def index
-    @bookings = Booking.all
+ def index
+  @bookings = Booking.all
+end
+
+def show
+  @booking = Booking.find(params[:id])
+end
+
+def new
+  @booking = Booking.new
+end
+
+def create
+  @booking = Booking.new(booking_params)
+  @booking.hooker = @hooker
+  @booking.user = current_user
+
+  if @booking.save
+    redirect_to hooker_booking_path(@hooker, @booking)
+  else
+    render :new
+  end
+end
+
+def edit
+  unless current_user == @booking.user
+    flash[:alert] = "You can't update an other person's booking"
+    redirect_to hooker_booking_path(@hooker, @booking)
+  end
+  @booking = Booking.find(params[:id])
+end
+
+def destroy
+  unless current_user == @booking.user
+    flash[:notice] = "You can't destroy a hooker that's not yours"
+    redirect_to hooker_booking_path(@hooker, @booking)
+  end
+  @booking.destroy
+  redirect_to hooker_booking_path(@hooker, @booking)
   end
 
-  def show
-    @booking = Booking.find(params[:id])
-  end
+private
 
-  def new
-    @booking = Booking.new
-  end
+def set_hooker
+  @hooker = Hooker.find(params[:hooker_id])
+end
 
-  def create
-    @booking = Booking.new(booking_params)
-    @booking.hooker = @hooker
-    @booking.user = current_user
-
-    if @booking.save
-      redirect_to hooker_booking_path(@hooker, @booking)
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def destroy
-    @booking.destroy
-    redirect_to hooker_path(@hooker)
-  end
-
-  private
-
-  def set_hooker
-    @hooker = Hooker.find(params[:hooker_id])
-  end
-
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :special)
-  end
+def booking_params
+  params.require(:booking).permit(:start_date, :end_date, :special)
+end
   #{}"start_date"=>"1333", "end_date(1i)"=>"2017", "end_date(2i)"=>"5", "end_date(3i)"=>"30", "end_date(4i)"=>"13", "end_date(5i)"=>"50"
 end
