feeb3283082fe9c72b5d3287b6f4373121c427a5
thibaut gendebien <thibautgendebien@gmail.com>
2017-05-30 14:00:47 +0200

    bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    resources :bookings
-    resources :hookers
+
+    resources :hookers do
+      resources :bookings
+    end
     resources :reviews
 end
