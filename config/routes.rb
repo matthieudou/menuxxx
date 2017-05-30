d4c7f84db40612a6a97fdf067b1eafcabe811628
thibaut gendebien <thibautgendebien@gmail.com>
2017-05-30 10:59:10 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- resources :users, only: [ :create, :new, :edit, ]  do
+
     resources :bookings
     resources :hookers
     resources :reviews
-  end
 end
