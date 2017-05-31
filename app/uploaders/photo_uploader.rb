02cb2f0db68478862df4f078cc1ca0270877ad15
matthieudou <matthieu.doultremont@gmail.com>
2017-05-31 15:09:57 +0200

    resolving the issue with error no photo in index and show -> setting default

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,8 @@
 # app/uploaders/photo_uploader.rb
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  def default_url
+    "https://www.w3schools.com/css/trolltunga.jpg"
+  end
 end
